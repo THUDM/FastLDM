b8d76a8e5859cb304754200dba1dfbe918164f49
Qingsong Lv <lqs@mail.bnu.edu.cn>
2023-03-06 07:16:11 +0000

    onnx hook

--- fastldm/experiment.py
+++ fastldm/experiment.py
@@ -10,25 +10,29 @@ from collections import Counter
 
 PLUGIN_config = ' '.join(['--plugins={}'.format(p) for p in PLUGINS])
 
-def generate_trt(model, inputs, kw_args={}, experiment_name='', onnx_only=False, use_fp16=True, skip=False):
+def generate_trt(model, inputs, kw_args={}, experiment_name='', onnx_only=False, use_fp16=True, skip=False, onnx_hook=None):
     os.makedirs('./onnx/', exist_ok=True)
     os.makedirs('./trt/', exist_ok=True)
     name = '{}_ONNX_ONLY_{}_{}'.format(type(model).__name__, ONNX_ONLY, experiment_name)
+    onnx_path = 'onnx/{}.onnx'.format(name)
+    trt_path = 'trt/{}.trt'.format(name)
     if skip:
         if onnx_only:
-            return 'onnx/{}.onnx'.format(name)
-        return 'onnx/{}.onnx'.format(name), 'trt/{}.trt'.format(name)
+            return onnx_path
+        return onnx_path, trt_path
     model.eval()
     with torch.no_grad():
         outputs = model(*inputs, **kw_args)
         num_output = 1 if not list_or_tuple(outputs) else len(outputs)
         os.system("rm onnx/{}.onnx".format(name))
-        torch.onnx.export(model, tuple(inputs)+(kw_args,), 'onnx/{}.onnx'.format(name), operator_export_type=OperatorExportTypes.ONNX_FALLTHROUGH, input_names=['input_{}'.format(i) for i in range(len(inputs)+len(kw_args))], output_names=['output_{}'.format(i) for i in range(num_output)])
+        torch.onnx.export(model, tuple(inputs)+(kw_args,), onnx_path, operator_export_type=OperatorExportTypes.ONNX_FALLTHROUGH, input_names=['input_{}'.format(i) for i in range(len(inputs)+len(kw_args))], output_names=['output_{}'.format(i) for i in range(num_output)])
+        if onnx_hook is not None:
+            onnx_hook(onnx_path)
         if onnx_only:
-            return 'onnx/{}.onnx'.format(name)
-    os.system("rm trt/{}.trt".format(name))
-    os.system("trtexec --onnx=onnx/{}.onnx --saveEngine=trt/{}.trt --buildOnly {} {}".format(name, name, '--fp16' if use_fp16 else '', PLUGIN_config))
-    return 'onnx/{}.onnx'.format(name), 'trt/{}.trt'.format(name)
+            return onnx_path
+    os.system("rm {}".format(trt_path))
+    os.system("trtexec --onnx={} --saveEngine={} --buildOnly {} {}".format(onnx_path, trt_path, '--fp16' if use_fp16 else '', PLUGIN_config))
+    return onnx_path, trt_path
 
 def experiment(models, trt_models, inputs, kw_args={}, n_iter=100, warm_up_step=5, forward_only=True, benchmark_func=benchmark):
     measure_dict = OrderedDict()
@@ -74,8 +78,8 @@ def experiment_onnx(model, inputs, kw_args={}, new_inputs=None, new_kw_args=None
         new_kw_args = kw_args
     return experiment([model, ortmodel], [], new_inputs, kw_args=new_kw_args, n_iter=1, warm_up_step=0)
 
-def experiment_trt(model, inputs, kw_args={}, new_inputs=None, new_kw_args=None, use_fp16=True, skip=False):
-    _, trt_path = generate_trt(model, inputs, kw_args=kw_args, use_fp16=use_fp16, skip=skip)
+def experiment_trt(model, inputs, kw_args={}, new_inputs=None, new_kw_args=None, use_fp16=True, skip=False, onnx_hook=None):
+    _, trt_path = generate_trt(model, inputs, kw_args=kw_args, use_fp16=use_fp16, skip=skip, onnx_hook=onnx_hook)
     trtmodel = TRTModule(trt_path, 1)
     if new_inputs is None:
         new_inputs = inputs
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='fastldm',
-    version='0.2.1',
+    version='0.2.2',
     description='',
     packages=['fastldm'],
     install_requires=[
